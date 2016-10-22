b30e06e07a5adb28be16726c64141c5cc68f8bc6
VikingPaul <wip321@yahoo.com>
2016-10-22 11:56:01 -0500

    angularJS set up, form made, angular factory up and running, everything connected. all that's lacking is the status code. and to know what exactly i'm doing.

---
+++ FetchNStore/FetchNStore/Scripts/app.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+var app = angular.module('fetchNStuff', []);
+
+
+app.controller("requests", ($scope, DOOMFactory) => {
+    $scope.request = {
+        method: "",
+        url: ""
+    };
+    $scope.sendRequest = () => {
+        DOOMFactory.request($scope.request);
+    };
+});
+
+
+app.factory("DOOMFactory", ($http, $q) => {
+    var request = (requestInfo) => {
+        return $q((resolve) => {
+            let startTime = Date.now();
+            $http({
+                method: requestInfo.method,
+                url: requestInfo.url
+            }).success((code) => {
+                let endTime = Date.now();
+                let total = endTime - startTime;
+                console.log(code, requestInfo.url, requestInfo.method, total);
+                resolve(code, requestInfo.url, requestInfo.method, total);
+            }).error((code) => {
+                let endTime = Date.now();
+                let total = endTime - startTime;
+                console.log(code, requestInfo.url, requestInfo.method, total);
+                resolve(total, code);
+            })
+        })
+    };
+    return { request: request };
+});
